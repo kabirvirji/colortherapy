fe3011bcb450d4c25e95744c42f3e97c5594b73c
Quentin V <quentin.vilchez@gmail.com>
2020-04-09 00:30:29 +0100

    Initial routing work

--- hexlist/src/App.js
+++ hexlist/src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
 import Login from "./components/Login/Login";
+import { Route } from "react-router-dom";
+import Quiz from "../src/components/Quiz/Quiz";
 
 class App extends Component {
   render() {
-    return <Login />;
+    return (
+      <div>
+        <Route path='/' exact component={Login} />
+        <Route path='/quiz' component={Quiz} />
+      </div>
+    );
   }
 }
 
---
+++ hexlist/src/components/Quiz/Quiz.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default class extends React.Component {
+  render() {
+    return <h1>Welcome to Quizz</h1>;
+  }
+}
