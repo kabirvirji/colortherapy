782752bec8b19847f6b9fa43eebddbadb2d599a9
kabirvirji <>
2020-04-10 22:02:11 -0700

    infinite scroll with custom reload

--- hexlist/src/components/ColorPicker/ColorPicker.js
+++ hexlist/src/components/ColorPicker/ColorPicker.js
@@ -1,66 +1,93 @@
 import React from "react";
 import Flex from '../Flex/Flex'
 import HexlistHeader from "../HexlistHeader/HexlistHeader";
+import InfiniteScroll from 'react-infinite-scroll-component';
 import "./ColorPicker.css";
-
-const numberOfSquares = 500 
 
 export default class extends React.Component {
     state = {
-        currentColor: '',
-        usedRGB: [],
-        chosenRGB: []
+        usedRGB: [], // looks like [{backgroundColor: color}]
+        chosenRGB: [], // looks like [["152", "33", "89"]]
+        numberOfSquares: 40,
+        refreshRate: 10 // squares per scroll
     };
     constructor(props) {
         super(props);
         this.randomRGB = this.randomRGB.bind(this)
-        // this.handlePick = this.handlePick.bind(this)
     }
-    randomRGB() { 
+    randomRGB() { // returns unique rgb color
         const rand = () => {
             return Math.floor(Math.random() * 255)
         }
         while (1) {
             let color = `rgb(${rand()}, ${rand()}, ${rand()})`
             if (!this.state.usedRGB.includes(color)) {
-                this.state.usedRGB.push(color)
-                return {
+                let formattedColor = {
                     backgroundColor: color
                 }
+                this.setState(prevState => ({
+                    usedRGB: [...prevState.usedRGB, formattedColor]
+                }))
+                return formattedColor
             }
         }
     
     }
     handlePick(e, color) {
-        if (!this.state.chosenRGB.includes(color)) {
-            let rgbArr = color.replace(/[^\d,]/g, '').split(',');
-            this.state.chosenRGB.push(rgbArr)
+        let rgbArr = color.replace(/[^\d,]/g, '').split(',');
+        if (!this.state.chosenRGB.includes(rgbArr)) {
+            this.setState(prevState => ({
+                chosenRGB: prevState.chosenRGB.concat([rgbArr])
+            }))
         }
         e.currentTarget.style = {
             backgroundColor: '#ffffff' // makes square disappear
         }
-        console.log(this.state)
     }
-    componentDidMount() {
-        //this.props.didMount();
+    fetchData() {
+        let freshColors = []
+        for (let i = 0; i < this.state.refreshRate; i++) {
+            freshColors[i] = this.randomRGB()
+        }
+        setTimeout(() => {
+            this.setState(prevState => ({
+                usedRGB: [...prevState.usedRGB, ...freshColors]
+            }))
+        }, 1500);
+    }
+    UNSAFE_componentWillMount() {
+        const init = () => {
+            let tmp = []
+            for (let i = 0; i < this.state.numberOfSquares; i++) {
+                tmp.push(this.randomRGB())
+            }
+            return tmp
+        }
+        this.setState({
+            usedRGB: init()
+        })
     }
     render() {
         return (
             <div>
                 <HexlistHeader></HexlistHeader>
-                <div className="colorContainer">
-                        <Flex flexWrap="wrap" justifyContent="center">
-                            {[...Array(numberOfSquares)].map(() => {
-                                let color = this.randomRGB()
-                                return (
-                                    <div className="square" style={color} onClick={(e) => this.handlePick(e, color.backgroundColor)} key={color.backgroundColor}>
-                                        <div className="inner"></div>
-                                    </div>
+                    <InfiniteScroll
+                        dataLength={this.state.usedRGB.length}
+                        next={() => this.fetchData()}
+                        hasMore={true}>
+                        <div className="colorContainer">
+                            <Flex flexWrap="wrap" justifyContent="center">
+                                {this.state.usedRGB.map((color, index) => {
+                                    return (
+                                        <div className="square" style={color} onClick={(e) => this.handlePick(e, color.backgroundColor)} key={index}>
+                                            <div className="inner"></div>
+                                        </div>
+                                    )}
                                 )}
-                            )}
-                        </Flex>
+                            </Flex>
+                        </div>
+                    </InfiniteScroll>
                 </div>
-            </div>
         );
     }
 }
