d853a3e7791cb2a91ba832e83fad47c898235329
Quentin V <quentin.vilchez@gmail.com>
2020-04-12 17:55:10 +0100

    Created some Quiz Cards

--- hexlist/src/components/Quiz/Quiz.js
+++ hexlist/src/components/Quiz/Quiz.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Quiz.css";
+import Button from "./Button/Button";
 
 export default class Quiz extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: 0,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
   componentDidMount() {
     this.props.didMount();
   }
+  handleClick(e) {}
   render() {
     return (
-      <div>
-        <h1>Welcome to Quizz</h1>
-        <Link
-          to={{
-            pathname: "/colorpicker",
-          }}>
-          ColorPicker
-        </Link>
+      <div id='container'>
+        <div className='card active'>
+          <p>
+            Before taking you to Color Therapy we'd like to ask you a few
+            questions to know how you're feeling.
+          </p>
+          <Button onClick={this.handleClick}>Next</Button>
+        </div>
+        <div className='card' id='1'>
+          something
+        </div>
       </div>
     );
   }
