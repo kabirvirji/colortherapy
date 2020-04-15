2d1872e5aac9c1484a24d79bf15e7cbfbc80b075
Quentin V <quentin.vilchez@gmail.com>
2020-04-15 23:52:28 +0100

    Added text to a few slides and changed button color

--- hexlist/src/components/Quiz/ColorQuestion/ColorQuestion.js
+++ hexlist/src/components/Quiz/ColorQuestion/ColorQuestion.js
@@ -7,44 +7,44 @@ import { Link } from "react-router-dom";
 function numberToWord(number) {
   if (number >= 0.9) {
     return (
-      <span className="emoji" role='img'>
+      <span className='emoji' role='img'>
         &#128525;
       </span>
     );
   }
   if (number >= 0.8 && number < 0.9) {
     return (
-      <span className="emoji" role='img' >
+      <span className='emoji' role='img'>
         &#128526;
       </span>
     );
   } else if (number >= 0.6 && number < 0.8) {
     return (
-      <span className="emoji" role='img' >
+      <span className='emoji' role='img'>
         &#128527;
       </span>
     );
   } else if (number >= 0.4 && number < 0.6) {
     return (
-      <span className="emoji" role='img' >
+      <span className='emoji' role='img'>
         &#128528;
       </span>
     );
   } else if (number >= 0.2 && number < 0.4) {
     return (
-      <span className="emoji" role='img' >
+      <span className='emoji' role='img'>
         &#128530;
       </span>
     );
   } else if (number >= 0.1 && number < 0.2) {
     return (
-      <span className="emoji" role='img' >
+      <span className='emoji' role='img'>
         &#128531;
       </span>
     );
   } else if (number >= 0 && number < 0.1) {
     return (
-      <span className="emoji" role='img' >
+      <span className='emoji' role='img'>
         &#128534;
       </span>
     );
@@ -70,7 +70,6 @@ export default class ColorQuestion extends React.Component {
               minText={"sad"}
               maxText={"happy"}
             />
-            
           </div>
           <Button onClick={this.props.onClick}>Next</Button>
         </div>
@@ -83,12 +82,15 @@ export default class ColorQuestion extends React.Component {
             id={this.props.id}
             style={{ background: this.props.id }}></div>
           <div className='range'>
+            {numberToWord(this.props.value)}
             <Range
               onChange={this.props.onChange}
               id={this.props.id}
               value={this.props.value}
+              // min and max text can also be emojis
+              minText={"sad"}
+              maxText={"happy"}
             />
-            {numberToWord(this.props.value)}
           </div>
           <Link to='/colorpicker'>
             <Button onClick={this.props.onClick}>Next</Button>
