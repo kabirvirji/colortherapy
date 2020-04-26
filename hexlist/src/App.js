e7c1b154a109b7e288ad83854296677d62035072
Quentin V <quentin.vilchez@gmail.com>
2020-04-26 22:31:08 +0100

    LOGIN false

--- hexlist/src/App.js
+++ hexlist/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      LOGIN: true,
+      LOGIN: false,
       energy: 0.5,
       red: 0.5,
       green: 0.5,
